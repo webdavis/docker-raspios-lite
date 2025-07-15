747af229c14bffc91778e2fba5825f495e0d1dcc
Stephen A. Davis <stephen@webdavis.io>
2025-07-14 23:50:02 -0600

    Store Raspberry Pi OS versions in a config file and then source it for easier tracking

--- docker-wrapper.sh
+++ docker-wrapper.sh
@@ -6,9 +6,11 @@ set -e
 # Ensure the entire pipeline fails if any command in the pipeline fails.
 set -o pipefail
 
+source "./.raspios-versions"
+
 docker buildx build \
   --load \
   --platform linux/arm64 \
   --no-cache \
-  --build-arg ROOTFS_URL=https://downloads.raspberrypi.com/raspios_lite_arm64/archive/2025-05-13-08:03/root.tar.xz \
+  --build-arg ROOTFS_URL=https://downloads.raspberrypi.com/raspios_lite_arm64/archive/${ARM64_VERSION}/root.tar.xz \
   --tag raspios-lite:latest .
